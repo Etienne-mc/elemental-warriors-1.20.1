a2ac991e6e247d0e5ad66dc7817aabf8276699d6
Etienne-mc <etiennejardin@hotmail.com>
2023-10-21 15:06:54 +0200

    initial commit

---
+++ src/main/java/net/etienne/elementalwarriors/ElementalWarriorsClient.java
@@ -0,0 +1,10 @@
+package net.etienne.elementalwarriors;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalWarriorsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
